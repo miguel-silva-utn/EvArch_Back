51db6651cec3f5e3cd998c77df5981b52e467f6e
Miguel Silva C. <miguel.silva@inspt.utn.edu.ar>
2025-06-07 13:33:56 -0300

    Evaluador de archivos C y Pseint - Backend - v1.0

--- api/index.js
+++ api/index.js
@@ -1,2 +1,23 @@
-import app from '../server.js';
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import evaluateRoutes from './routes/evaluateRoutes.js';
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/evaluate', evaluateRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Servidor activo ✅');
+});
+
+// Exporta como función para Vercel:
 export default app;
+
+// Verifica que el servidor responde en server.js:
+app.get('/api/ping', (req, res) => {
+    res.send('Servidor activo desde Vercel');
+});
