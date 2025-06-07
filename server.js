785470332b1948730fb9872cdc297f54d0bb79c2
Miguel Silva C. <miguel.silva@inspt.utn.edu.ar>
2025-06-07 13:40:25 -0300

    Evaluador de archivos C y Pseint - Backend - v1.0

--- server.js
+++ server.js
@@ -10,17 +10,9 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/evaluate', evaluateRoutes);
 
-//const PORT = process.env.PORT || 5000;
-//app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
 
 app.get('/', (req, res) => {
     res.send('Servidor activo ✅');
-});
-
-// Exporta como función para Vercel:
-export default app;
-
-// Verifica que el servidor responde en server.js:
-app.get('/api/ping', (req, res) => {
-    res.send('Servidor activo desde Vercel');
 });
